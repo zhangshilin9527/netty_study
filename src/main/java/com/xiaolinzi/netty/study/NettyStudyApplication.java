cf5806a09d3e624f3224960bf334fad90e93142a
zhangshilin9527 <aa930301>
2021-03-14 23:25:14 +0800

    初始化代码

---
+++ src/main/java/com/xiaolinzi/netty/study/NettyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.xiaolinzi.netty.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaolinzi/netty/study/NettyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaolinzi.netty.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
